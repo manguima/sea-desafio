0b00b6689b6bf0e1a4d3e6fad8c568e6d58219ad
Matheus Guimarães <dredhors@hotmail.com>
2024-12-10 09:04:58 -0300

    first commit

---
+++ modules/react-dashboard/src/main/java/com/example/react/dashboard/constants/ReactDashboardPortletKeys.java
@@ -0,0 +1,11 @@
+package com.example.react.dashboard.constants;
+
+/**
+ * @author BEST OPTION NOTEBOOK
+ */
+public class ReactDashboardPortletKeys {
+
+	public static final String REACTDASHBOARD =
+		"com_example_react_dashboard_ReactDashboardPortlet";
+
+}
