275b60ce3dc373a6af056c6a0000290b3cb3e53a
Matheus Guimarães <dredhors@hotmail.com>
2024-12-15 18:18:01 -0300

    feat: adiciona funcionalidades e json server

--- app/funcionario/lista/page.tsx
+++ app/funcionario/lista/page.tsx
@@ -1,8 +1,24 @@
 "use client";
+import { Shortly } from "@/src/components/default/shortly/Shortly";
+import { DescEmployee } from "@/src/components/employee/desc/DescEmployee";
 import { ListEmployee } from "@/src/components/employee/list/ListEmployee";
+import { useEmployeeContext } from "@/src/providers/employee/EmployeeProvider";
 
 const EmployeeListPage = () => {
-  return <ListEmployee />;
+  const { stepCurrent } = useEmployeeContext();
+
+  return (
+    <>
+      {{
+        0: (
+          <>
+            <DescEmployee />
+            <ListEmployee />
+          </>
+        ),
+      }[stepCurrent] || <Shortly />}
+    </>
+  );
 };
 
 export default EmployeeListPage;
--- src/components/employee/list/item/ListItemEmployee.tsx
+++ src/components/employee/list/item/ListItemEmployee.tsx
@@ -1,15 +1,52 @@
 "use client";
 
-import { Button, Flex, Popover, Tag, Typography } from "antd";
+import { Button, Divider, Flex, Popover, Tag, Typography } from "antd";
 
 import styles from "./list-item-employee.module.css";
 import { DotsIcon } from "@/src/assets/dots-icon";
 import { Employee } from "@/src/types/FormEmployeeProps";
 import { jobPositionsList } from "@/src/config/job";
 import Link from "next/link";
+import { useAppDispatch } from "@/src/hooks/useSeaAppSelector";
+import { deleteEmployee } from "@/src/redux/slices/employeeSlice";
+import { useModal } from "@/src/components/default/modal/Modal";
 
 export const ListItemEmployee = ({ employee }: { employee: Employee }) => {
-  const { Text } = Typography;
+  const { Text, Title } = Typography;
+
+  const dispatch = useAppDispatch();
+
+  const { showModal } = useModal();
+
+  const handleDeleteUser = () => {
+    // Disparar a ação para excluir o funcionário
+    dispatch(deleteEmployee(employee.id as string)).then(() => {
+      showModal({
+        title: "",
+        content: "Usuário excluído com sucesso!",
+        onOk: () => console.log("Confirmed"),
+        onCancel: () => console.log("Cancelled"),
+      });
+    });
+  };
+
+  const suboptions = (
+    <Flex vertical className={styles.popover}>
+      <Link href={`/funcionario/${employee.id}`}>
+        <Button style={{ background: "transparent" }} className={styles.btn}>
+          Editar
+        </Button>
+      </Link>
+      <Divider style={{ margin: "10px 0", padding: 0 }} />
+      <Button
+        onClick={handleDeleteUser}
+        style={{ background: "transparent" }}
+        className={styles.btn}
+      >
+        Excluir
+      </Button>
+    </Flex>
+  );
 
   return (
     <Flex className={`${styles.card}`}>
@@ -29,7 +66,12 @@ export const ListItemEmployee = ({ employee }: { employee: Employee }) => {
         </Flex>
       </Flex>
       <Flex className={styles.options}>
-        <Popover content={suboptions} title="Title" trigger="focus">
+        <Popover
+          placement="left"
+          arrow={false}
+          color="white"
+          content={suboptions}
+        >
           <a className={styles.btn}>
             <DotsIcon
               w="16px"
@@ -40,16 +82,5 @@ export const ListItemEmployee = ({ employee }: { employee: Employee }) => {
         </Popover>
       </Flex>
     </Flex>
-  );
-};
-
-const suboptions = () => {
-  return (
-    <div>
-      <Link href={"#"}>
-        <Button>Editar</Button>
-      </Link>
-      <Button>Excluir</Button>
-    </div>
   );
 };
