953a3b4359e949851459582c424ca8907b2f0384
Matheus Guimarães <dredhors@hotmail.com>
2024-12-15 20:15:55 -0300

    feat: adiciona "em-breve" na pagina inicial

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Shortly } from "@/src/components/default/shortly/Shortly";
+
 export default function Home() {
-  return <div style={{ width: "100%", height: "100%" }}></div>;
+  return (
+    <div style={{ width: "100%", height: "100%" }}>
+      <Shortly />
+    </div>
+  );
 }
